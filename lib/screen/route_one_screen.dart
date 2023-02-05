b1b078929a9ce25cbc9f9b36f8ef951e18abdf1a
namseungsik <tmd7031@naver.com>
2023-02-05 22:02:20 +0900

    argumemts 사용한 값 전달

--- lib/screen/route_one_screen.dart
+++ lib/screen/route_one_screen.dart
@@ -1,20 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:navigation/layout/main_layout.dart';
-
+import 'package:navigation/screen/route_two_screen.dart';
 
 class RouteOneScreen extends StatelessWidget {
-  const RouteOneScreen({Key? key}) : super(key: key);
+  final int number;
+  const RouteOneScreen({
+    Key? key,
+    required this.number,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MainLayout(
       title: 'Route One',
       children: [
+        Text(
+          'argumemts : ${number.toString()}',
+          textAlign: TextAlign.center,
+        ),
         ElevatedButton(
           onPressed: () {
-            Navigator.of(context).pop();
+            Navigator.of(context).pop(456);
           },
           child: Text('Pop'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) => RouteTwoScreen(),
+                settings: RouteSettings(
+                  arguments: 789,
+                )
+              ),
+            );
+          },
+          child: Text('Push'),
         ),
       ],
     );
--- lib/screen/route_two_screen.dart
+++ lib/screen/route_two_screen.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:navigation/layout/main_layout.dart';
 
 class RouteTwoScreen extends StatelessWidget {
   const RouteTwoScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('RouteTwoScreen'),
-      ),
+    // ModalRoute.of(context) : build안에 있기 때문에 RouteTwoScreen 실행되어있기 때문에 RouteTwoScreen의 값을 가져오는 듯
+    final arguments = ModalRoute.of(context)!.settings!.arguments;
+    return MainLayout(
+      title: 'Route Two',
+      children: [
+        Text(
+          'argumemts : ${arguments}',
+          textAlign: TextAlign.center,
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text('Pop'),
+        )
+      ],
     );
   }
 }
