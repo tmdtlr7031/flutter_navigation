080b34b9d11842bd65f081cbf13a05df9054d06e
namseungsik <tmd7031@naver.com>
2023-02-05 22:59:25 +0900

    pop 메서드 + 안드로이트 back 버튼 방지

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -7,24 +7,56 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MainLayout(
-      title: 'Home Screen',
-      children: [
-        ElevatedButton(
-          onPressed: () async {
-            final result = await Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (BuildContext context) => RouteOneScreen(
-                  number: 123,
-                ),
-              ),
-            );
+    /**
+     * WillPopScope : 안드로이드 back 버튼 시 앱꺼짐 방지
+     *  - 시스템적으로 back 버튼 눌렀을 때 리스닝할 수 있는 방법은 없음.
+     *  - WillPopScope -> 라우트가 사라질 때 이벤트 캐치
+     */
+    return WillPopScope(
+      onWillPop: () async { // async 무조건 써야함
+        // return = true : pop 가능
 
-            print(result);
-          },
-          child: Text('push'),
-        ),
-      ],
+        // 로직 추가 가능
+
+        return false;
+      },
+      child: MainLayout(
+        title: 'Home Screen',
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              print(Navigator.of(context).canPop()); // 마지막 페이지인 경우 false 리턴
+            },
+            child: Text('Can Pop'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).maybePop(); // 뒤로갈 페이지가 없을 때 뒤로가기 방지
+            },
+            child: Text('Maybe Pop'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('Pop'),
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              final result = await Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (BuildContext context) => RouteOneScreen(
+                    number: 123,
+                  ),
+                ),
+              );
+
+              print(result);
+            },
+            child: Text('push'),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/screen/route_one_screen.dart
+++ lib/screen/route_one_screen.dart
@@ -20,6 +20,12 @@ class RouteOneScreen extends StatelessWidget {
         ),
         ElevatedButton(
           onPressed: () {
+            Navigator.of(context).maybePop(); // 뒤로갈 페이지가 없을 때 뒤로가기 방지
+          },
+          child: Text('Maybe Pop'),
+        ),
+        ElevatedButton(
+          onPressed: () {
             Navigator.of(context).pop(456);
           },
           child: Text('Pop'),
