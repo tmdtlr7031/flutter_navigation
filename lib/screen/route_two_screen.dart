eb894db80714d77f459df661ae8a442294c20cf3
namseungsik <tmd7031@naver.com>
2023-02-05 22:40:10 +0900

    push 메서드 + 뒤로가기 방지때 참고하면 좋을 듯

--- lib/screen/route_two_screen.dart
+++ lib/screen/route_two_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:navigation/layout/main_layout.dart';
+import 'package:navigation/screen/route_three_screen.dart';
 
 class RouteTwoScreen extends StatelessWidget {
   const RouteTwoScreen({Key? key}) : super(key: key);
@@ -29,6 +30,33 @@ class RouteTwoScreen extends StatelessWidget {
             );
           },
           child: Text('Push Named'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            // 뒤로가기 방지 가능
+            // Navigator.of(context).pushReplacement(
+            //   MaterialPageRoute(builder: (_) => RouteThreeScreen(),
+            //   ),
+            // );
+            Navigator.of(context)
+                .pushReplacementNamed('/three'); // 바로 위에 주석된 소스와 같은 의미
+          },
+          child: Text('Push Replacement'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            // Navigator.of(context).pushNamedAndRemoveUntil('/three', (route) => route.settings.name == '/',); 같은 의미
+            Navigator.of(context).pushAndRemoveUntil(
+              MaterialPageRoute(builder: (_) => RouteThreeScreen()),
+                /**
+                 *  (route) => false : (route) 에 모든 스크린 스택이 차례로 들어가게되고 false 면 스크린 삭제 -> 따라서 모든 스크린이 삭제되어 마지막 페이지에서 뒤로가기하면 검은화면
+                 *  (route) => true : 페이지 삭제 안함
+                 *  (route) => route.settings.name == null : named 함수를 사용 안한 경우 null 나옴
+                 */
+              (route) => route.settings.name == '/', // 홈스크린 빼고 삭제
+            );
+          },
+          child: Text('Push And Remove Until'),
         ),
       ],
     );
