82aeebc4cc443b37f88bf8ef65bd4eecb4f88211
namseungsik <tmd7031@naver.com>
2023-02-05 22:17:48 +0900

    Named Route 사용

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:navigation/screen/home_screen.dart';
+import 'package:navigation/screen/route_one_screen.dart';
+import 'package:navigation/screen/route_three_screen.dart';
+import 'package:navigation/screen/route_two_screen.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: HomeScreen(),
+    // home: HomeScreen(),
+    initialRoute: '/', // 첫실행 설정
+    routes: {
+      '/': (context) => HomeScreen(),
+      '/one': (context) => RouteOneScreen(),
+      '/two': (context) => RouteTwoScreen(),
+      '/three': (context) => RouteThreeScreen(),
+    },
   ));
 }
